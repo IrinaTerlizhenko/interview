433497e7c4b4469e38cab2e2e419416cebbd9d8d
Iryna.Tserlizhenka <iryna.tserlizhenka@epicgames.com>
2020-06-18 21:11:41 +0300

    Remove possibility to hack this exercise

--- src/main/java/com/example/demo/stuff/MainObject.java
+++ src/main/java/com/example/demo/stuff/MainObject.java
@@ -2,12 +2,20 @@ package com.example.demo.stuff;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class MainObject {
 
-    private final SomeObject someObject;
+    private SomeObject someObject;
 
     public MainObject(SomeObject someObject) {
         this.someObject = someObject;
+    }
+
+    @PostConstruct
+    public void m()
+    {
+        System.out.println(someObject);
     }
 }
